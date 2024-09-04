e7653e6b614102eaad9829938aaa042c2b9f3d9f
zouyos <hamza.benketaf@gmail.com>
2024-09-05 01:09:55 +0200

    css

--- src/components/DropDownButton/DropDownButton.jsx
+++ src/components/DropDownButton/DropDownButton.jsx
@@ -7,7 +7,7 @@ import {
 
 export default function DropDownButton({ onClick }) {
   return (
-    <Menu __demoMode>
+    <Menu>
       <MenuButton className='inline-flex items-center gap-2 rounded-md bg-gray-500 py-2 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-500 data-[focus]:outline-1 data-[focus]:outline-white'>
         GitHub
         <ChevronDownIcon className='size-4 fill-white/60' />
