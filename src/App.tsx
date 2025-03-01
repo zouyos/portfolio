db2b7f67f0a16dccfe90a6b0d7535a07a7b4ba58
zouyos <hamza.benketaf@gmail.com>
2025-03-01 16:15:43 +0100

    projects display order rebased

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,9 @@ function App() {
   const { themeMode, setThemeMode } = useThemeModeContext();
 
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
-  const sortedProjects = [...projects].sort((a: Project, b: Project) => {
-    return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-  });
+  // const sortedProjects = [...projects].sort((a: Project, b: Project) => {
+  //   return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+  // });
 
   function handleProjectClick(project: Project | null) {
     setSelectedProject(project);
@@ -193,7 +193,7 @@ function App() {
             <hr />
             {projects.length > 0 && (
               <ProjectList
-                projects={sortedProjects}
+                projects={projects}
                 selectedProject={selectedProject}
                 onClick={handleProjectClick}
               />
